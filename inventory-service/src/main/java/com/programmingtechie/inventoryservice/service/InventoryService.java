10bf2d1833ce154dfb31bbc209d36d7d1f738422
Suhaib Khater <54662871+suhaibkhater98@users.noreply.github.com>
2024-01-26 15:54:30 -0600

    finish communcate betweem order and inventory services as sync communcate

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -1,9 +1,13 @@
 package com.programmingtechie.inventoryservice.service;
 
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.model.Inventory;
 import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,23 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+                .stream()
+                .map(inventory -> {
+                    return InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .inStock(inventory.getQuantity() > 0)
+                            .build();
+                }).toList();
+    }
+
+    public List<InventoryResponse> getAllInventories() {
+        return inventoryRepository.findAll().stream().map(
+                inventory -> InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .inStock(inventory.getQuantity() > 0)
+                        .build()
+        ).toList();
     }
 }
